66283bb40434d5fb029a83340c928101885689b1
Chien C. Lin <linc@NDEV.COIC.MIL>
2018-07-19 15:46:18 -0400

    updated getrules.sh

--- getrules.sh
+++ getrules.sh
@@ -1,5 +1,7 @@
 rm rules
-curl -X GET 'http://localhost:9000/api/rules/search?ps=500&p=1' -H 'api_key: 441acc6a21065be641f021ba040c701c6d468d62' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -X GET 'http://localhost:9000/api/rules/search?ps=500&p=2' -H 'api_key: 441acc6a21065be641f021ba040c701c6d468d62' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -X GET 'http://localhost:9000/api/rules/search?ps=500&p=3' -H 'api_key: 441acc6a21065be641f021ba040c701c6d468d62' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -X GET 'http://localhost:9000/api/rules/search?ps=500&p=4' -H 'api_key: 441acc6a21065be641f021ba040c701c6d468d62' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+
+curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=1' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=2' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=3' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=4' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=5' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
