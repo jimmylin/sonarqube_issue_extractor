5859a3826b5664f726c7dc4506ad3378cd8ca2a9
Chien C. Lin <linc@NDEV.COIC.MIL>
2018-07-19 15:48:06 -0400

    updated hostname

--- getrules.sh
+++ getrules.sh
@@ -1,7 +1,7 @@
 rm rules
 
-curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=1' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=2' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=3' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=4' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
-curl -k -X GET 'https://sonar.ndev.coic.mil/api/rules/search?ps=500&p=5' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://<hostname>/api/rules/search?ps=500&p=1' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://<hostname>/api/rules/search?ps=500&p=2' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://<hostname>/api/rules/search?ps=500&p=3' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://<hostname>/api/rules/search?ps=500&p=4' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
+curl -k -X GET 'https://<hostname>/api/rules/search?ps=500&p=5' -H 'api_key: <INSERT API KEY FROM SONARQUBE>' | jq -r '(.rules[] | [.key, .name, .severity, .status, .lang, .type]) | @csv' >> rules
